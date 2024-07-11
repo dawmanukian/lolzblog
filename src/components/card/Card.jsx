dae9ac9f6adbeb84788f1bc9642298d9493e11f3
dawitmanukian <davit.manukyan.d@tumo.org>
2024-07-11 21:49:55 +0400

    cards updates

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -10,6 +10,22 @@ const Card = ({ img, text, header }) => {
         alt="card_img"
         loading="lazy"
       />
+      <div className={classes.card_text}>
+        <span className={classes.card_header}>Blog</span>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ipsam
+          earum cum quibusdam dolor doloribus ducimus qui eius iusto placeat
+          harum, commodi error sapiente repellat ex dolorum nihil. Amet,
+          inventore? Nam unde minima tempora temporibus ullam nostrum facere
+          enim possimus laboriosam eveniet aspernatur, necessitatibus ipsum
+          saepe fugit dolores libero id obcaecati dicta, illo quibusdam. Nobis
+          cupiditate quo porro accusamus veniam!
+        </span>
+        <div className={classes.footer_card}>
+          <div>0 Comments</div>
+          <div>3 Likes</div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import classes from './blogs.module.css'
-import Card from '../../components/card/Card'
+import React from "react";
+import classes from "./blogs.module.css";
+import Card from "../../components/card/Card";
 
 const Blogs = () => {
   return (
     <div className={classes.page}>
-        <h1>Список статей</h1>
-        <div className={classes.cards}>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-        </div>
+      <h1>Список статей</h1>
+      <div className={classes.cards}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <div className={classes.btns}>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Blogs
+export default Blogs;
