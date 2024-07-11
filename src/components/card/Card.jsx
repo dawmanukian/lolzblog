746d8eb21dc71652c393abf9706c9e6e539b576d
dawitmanukian <davit.manukyan.d@tumo.org>
2024-07-12 01:36:46 +0400

    update

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,32 +1,40 @@
 import React from "react";
 import classes from "./card.module.css";
 
-const Card = ({ img, text, header }) => {
+const Card = ({ img, text, header, itsAdmin }) => {
   return (
-    <div className={classes.card}>
-      <img
-        className={classes.card_img}
-        src="https://st2.depositphotos.com/4107269/7704/i/450/depositphotos_77040729-stock-photo-vacation-entertainment-concept.jpg"
-        alt="card_img"
-        loading="lazy"
-      />
-      <div className={classes.card_text}>
-        <span className={classes.card_header}>Blog</span>
-        <span>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ipsam
-          earum cum quibusdam dolor doloribus ducimus qui eius iusto placeat
-          harum, commodi error sapiente repellat ex dolorum nihil. Amet,
-          inventore? Nam unde minima tempora temporibus ullam nostrum facere
-          enim possimus laboriosam eveniet aspernatur, necessitatibus ipsum
-          saepe fugit dolores libero id obcaecati dicta, illo quibusdam. Nobis
-          cupiditate quo porro accusamus veniam!
-        </span>
-        <div className={classes.footer_card}>
-          <div>0 Comments</div>
-          <div>3 Likes</div>
+    <>
+      <div className={classes.card}>
+        <img
+          className={classes.card_img}
+          src="https://st2.depositphotos.com/4107269/7704/i/450/depositphotos_77040729-stock-photo-vacation-entertainment-concept.jpg"
+          alt="card_img"
+          loading="lazy"
+        />
+        <div className={classes.card_text}>
+          <span className={classes.card_header}>Blog</span>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+            ipsam earum cum quibusdam dolor doloribus ducimus qui eius iusto
+            placeat harum, commodi error sapiente repellat ex dolorum nihil.
+            Amet, inventore? Nam unde minima tempora temporibus ullam nostrum
+            facere enim possimus laboriosam eveniet aspernatur, necessitatibus
+            ipsum saepe fugit dolores libero id obcaecati dicta, illo quibusdam.
+            Nobis cupiditate quo porro accusamus veniam!
+          </span>
+          <div className={classes.footer_card}>
+            <div>0 Comments</div>
+            <div>3 Likes</div>
+          </div>
         </div>
       </div>
-    </div>
+      {itsAdmin && (
+        <div className={classes.admin_btns}>
+          <span style={{ color: "red" }}>Удалить</span>
+          <span style={{ color: "orange" }}>Редактировать</span>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import classes from './admin.module.css'
+import React from "react";
+import classes from "./admin.module.css";
+import Card from "../../components/card/Card";
 
-const Admin = ({email}) => {
+const Admin = ({ email }) => {
   return (
     <div className={classes.page}>
-        <p>Welcome Admin {email}</p>
+      <p>{email}</p>
+      <hr />
+      <button className={classes.add_new}>Добавить статью</button>
+      <div className={classes.cards}>
+        <Card itsAdmin={true}/>
+        <Card itsAdmin={true}/>
+        <Card itsAdmin={true}/>
+        <Card itsAdmin={true}/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
